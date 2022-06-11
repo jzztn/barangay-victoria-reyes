1eaf1619f7bb068e12c05618b8ff5f96ac68854e
ntabucejo <ntabucejo@gmail.com>
2022-06-11 14:33:25 +0800

    Add next auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../adapters/prisma'
+
+export default NextAuth({
+  secret: process.env.SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+})
