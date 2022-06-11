903a7a2c57d0f4b232ece3f2692226dde83cefa7
ntabucejo <ntabucejo@gmail.com>
2022-06-11 19:10:20 +0800

    Setup store and auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,27 @@
-import { useSession, signIn, signOut } from 'next-auth/react'
+import { useSession, signIn } from 'next-auth/react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Component() {
   const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user!.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+  const router = useRouter()
+
+  const authenticate = async () => {
+    const response = await fetch(`/api/users/search/${session!.user!.email}`)
+    const data = await response.json()
+    router.push(`/${data.id}`)
   }
+
+  useEffect(() => {
+    if (session) authenticate()
+  }, [session])
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    session === null && (
+      <div>
+        Not signed in <br />
+        <button onClick={() => signIn('google')}>Sign in</button>
+      </div>
+    )
   )
 }
