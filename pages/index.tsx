21ac6af79fee78b5c4806dd9347797d8aa44eda4
ntabucejo <ntabucejo@gmail.com>
2022-06-12 01:06:37 +0800

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,24 @@
 import { useSession, signIn } from 'next-auth/react'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { NextPage } from 'next'
 
-export default function Component() {
+const LadingPage: NextPage = () => {
   const { data: session } = useSession()
   const router = useRouter()
 
-  const authenticate = async () => {
-    const response = await fetch(`/api/users/search/${session!.user!.email}`)
-    const data = await response.json()
-    router.push(`/${data.id}`)
-  }
-
   useEffect(() => {
-    if (session) authenticate()
+    if (!session) return
+    router.push(`/user/${String(session.user!.email!.split('@').at(0))}`)
   }, [session])
 
+  if (session !== null) return <>Loadin...</>
+
   return (
-    session === null && (
-      <div>
-        Not signed in <br />
-        <button onClick={() => signIn('google')}>Sign in</button>
-      </div>
-    )
+    <div>
+      <button onClick={() => signIn('google')}>Sign in</button>
+    </div>
   )
 }
+
+export default LadingPage
