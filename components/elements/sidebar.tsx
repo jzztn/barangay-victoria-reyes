64ebfb6068d82d240bf8545ebfdfba6e0aec8079
jazztinecruz <jzztn.crz@gmail.com>
2022-06-12 23:04:41 +0800

    Fix sidebar bug

--- components/elements/sidebar.tsx
+++ components/elements/sidebar.tsx
@@ -6,7 +6,7 @@ interface IProps {
 }
 const Sidebar = ({ children, close }: IProps) => {
   return (
-    <div className="h-full w-64 fixed right-0 top-0 bg-white py-6 px-8">
+    <div className="md:hidden h-full w-64 fixed right-0 top-0 bg-white py-6 px-8">
       <button className="bg-white w-10 h-10 shadow-gray/50 shadow-md rounded-full grid place-items-center">
         <ChevronDoubleRightIcon
           className="w-5 h-5 hover:text-primary"
