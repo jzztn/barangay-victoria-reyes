ddf3bb7ce6515eb4dfbd7f9683e13f704f4a611d
jazztinecruz <jzztn.crz@gmail.com>
2022-06-12 22:10:18 +0800

    Add colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0AA435",
+        gray: "#A8A7A7"
+      },
+      fontFamily: {
+        'hero': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
