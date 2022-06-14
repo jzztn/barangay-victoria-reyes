00642964efa612bfc0a295885a7c847633d8f367
jazztinecruz <jzztn.crz@gmail.com>
2022-06-15 00:58:02 +0800

    Add filter component

---
+++ components/elements/filter.tsx
@@ -0,0 +1,44 @@
+import { Listbox } from '@headlessui/react'
+import { CheckIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+import Dropdown from '../styled/dropdown'
+
+const Filter = () => {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+  const [categories] = useState([
+    'cedula',
+    'barangay certificate',
+    'clearance certificate',
+    'certificate of indigency',
+  ])
+  return (
+    <Dropdown
+      value={selectedCategory}
+      onChange={setSelectedCategory}
+      buttonName={selectedCategory}
+    >
+      {categories.map((category, index) => (
+        <Listbox.Option
+          key={index}
+          className="hover:bg-primary/40 hover:text-primary text-sm cursor-pointer"
+          value={category}
+        >
+          {({ active, selected }) => (
+            <h3
+              className={`${
+                active ? 'bg-primary/40 text-primary' : 'text-black'
+              } flex items-center gap-3 p-3 whitespace-nowrap`}
+            >
+              {selected && (
+                <CheckIcon className="bg-primary/30 rounded-full p-1 text-primary w-5 h-5" />
+              )}
+              {category}
+            </h3>
+          )}
+        </Listbox.Option>
+      ))}
+    </Dropdown>
+  )
+}
+
+export default Filter
