9f09fd86056ec5081ee4f919b3066be4af6e61ad
ntabucejo <ntabucejo@gmail.com>
2022-06-19 01:45:23 +0800

    Update ticket

--- stores/use-user-store.ts
+++ stores/use-user-store.ts
@@ -1,9 +1,5 @@
 import type { Profile, Resident, User } from '../prisma/definition'
 import create from 'zustand'
-<<<<<<< HEAD
-import { Ticket } from '@prisma/client'
-=======
->>>>>>> 6127e95ce1ac5e790630a33e4622c37de8a308e3
 
 interface UseUserStore {
   user: User | null
@@ -16,25 +12,6 @@ interface UseUserStore {
   create: {
     profile: (payload: { profile: Profile }) => void
     record: (payload: { record: Resident }) => void
-<<<<<<< HEAD
-    ticket: (payload: { ticket: Ticket }) => void
-  }
-}
-
-interface UseUserStore {
-  user: User | null
-  read: (payload: { user: User }) => void
-  unRead: () => void
-  update: {
-    user: (payload: { key: 'authorized'; value: boolean }) => void
-    profile: (payload: { profile: Profile }) => void
-  }
-  create: {
-    profile: (payload: { profile: Profile }) => void
-    record: (payload: { record: Resident }) => void
-    ticket: (payload: { ticket: Ticket }) => void
-=======
->>>>>>> 6127e95ce1ac5e790630a33e4622c37de8a308e3
   }
 }
 
@@ -78,23 +55,6 @@ const useUserStore = create<UseUserStore>((set, get) => ({
         method: 'POST',
         body: JSON.stringify(record),
       })
-<<<<<<< HEAD
-    },
-    ticket: async ({ ticket }) => {
-      set(({ user }) => ({
-        user: {
-          ...user!,
-          authorized: false,
-          tickets: [...user!.tickets!, ticket],
-        },
-      }))
-      const { id } = get().user!
-      await fetch(`/api/users/${id}/ticket/create`, {
-        method: 'POST',
-        body: JSON.stringify(ticket),
-      })
-=======
->>>>>>> 6127e95ce1ac5e790630a33e4622c37de8a308e3
     },
   },
 }))
