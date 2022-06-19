ecf4553c75a91611bb9d1f63672f112d6e001376
jazztinecruz <jzztn.crz@gmail.com>
2022-06-20 00:45:59 +0800

    Add request document type props

--- components/section/request/index.tsx
+++ components/section/request/index.tsx
@@ -1,7 +1,11 @@
+import { User } from '../../../prisma/definition'
 import Cards from '../cards'
 import Card from '../cards/card'
 
-const RequestDocuments = () => {
+interface IProps {
+  user:User
+}
+const RequestDocuments = ({user}: IProps) => {
   return (
     <div className="grid grid-rows-[auto,1fr,auto] gap-5 px-10 py-7">
       <h1 className="font-semibold tracking-wide">Request Documents</h1>
@@ -11,21 +15,32 @@ const RequestDocuments = () => {
             title="Barangay Certificate"
             paragraph="a document certifies that you are a good resident in the barangay and have a good moral character. It also signifies that you do not have any negative record and this is necessary in any official transactions."
             request={true}
+            type="barangay certificate"
+            id={user.id}
           />
           <Card
             title="Clearance Certificate"
             paragraph="a requirement before the municipality issues any license for the bu-siness or activity. The barangay clearance is obtained from the barangay where the intended business of activity is located or conducted."
             request={true}
+            type="clearance certificate"
+            id={user.id}
+
           />
           <Card
             title="Cedula"
             paragraph="a document issued to Filipinos upon payment of a residence tax. It can also serve as valid identification for individuals and corporations residing or located in the same municipality where it is acquired."
             request={true}
+            type="cedula"
+            id={user.id}
+
           />
           <Card
             title="Certificate of Indigency"
             paragraph="is issued to less fortunate resident who desires to avail assistance such as Scholarship, Medical Services, Free Legal Aid from Public Attorney's Office (PAO) and the like."
             request={true}
+            type="certificate of indigency"
+            id={user.id}
+
           />
         </Cards>
       </div>
