d7cac1968945db3f39ade4963d42150a0c09174e
jazztinecruz <jzztn.crz@gmail.com>
2022-06-20 00:49:41 +0800

    Add user update api

---
+++ pages/api/users/[userId]/update.ts
@@ -0,0 +1,24 @@
+import type { NextApiHandler } from "next";
+import prisma from "../../../../adapters/prisma";
+
+const handler: NextApiHandler = async (request, response) => {
+if (request.method !== "PUT") {
+response.status(500).json("Invalid request method");
+}
+try {
+const id = String(request.query.userId);
+const body = JSON.parse(request.body);
+const user = await prisma.user.update({
+where: { id: id },
+data: {
+[body.key]: body.value,
+},
+});
+response.status(201).json(user);
+} catch (error) {
+console.log(error);
+response.status(500).json(error);
+}
+};
+
+export default handler;
