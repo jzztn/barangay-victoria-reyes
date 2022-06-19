fcebeaae835cbd70cfa18cb4edcfdb9a0b70aa0f
jazztinecruz <jzztn.crz@gmail.com>
2022-06-20 01:05:31 +0800

    Add update user api

--- pages/api/users/[userId]/update.ts
+++ pages/api/users/[userId]/update.ts
@@ -1,24 +1,25 @@
-import type { NextApiHandler } from "next";
-import prisma from "../../../../adapters/prisma";
+import type { NextApiHandler } from 'next'
+import prisma from '../../../../adapters/prisma'
 
 const handler: NextApiHandler = async (request, response) => {
-if (request.method !== "PUT") {
-response.status(500).json("Invalid request method");
+  if (request.method !== 'PUT') {
+    response.status(500).json('Invalid request method')
+  }
+  try {
+    const id = String(request.query.userId)
+    const body = JSON.parse(request.body)
+    console.log(body)
+    const user = await prisma.user.update({
+      where: { id: id },
+      data: {
+        [body.key]: body.value,
+      },
+    })
+    response.status(201).json(user)
+  } catch (error) {
+    console.log(error)
+    response.status(500).json(error)
+  }
 }
-try {
-const id = String(request.query.userId);
-const body = JSON.parse(request.body);
-const user = await prisma.user.update({
-where: { id: id },
-data: {
-[body.key]: body.value,
-},
-});
-response.status(201).json(user);
-} catch (error) {
-console.log(error);
-response.status(500).json(error);
-}
-};
 
-export default handler;
+export default handler
