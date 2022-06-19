a543fe7464dade5f76aee6a2aa4e1988b6266749
jazztinecruz <jzztn.crz@gmail.com>
2022-06-20 01:43:07 +0800

    Add request button functionality

--- components/section/cards/card.tsx
+++ components/section/cards/card.tsx
@@ -7,29 +7,42 @@ interface IProps {
   title: string
   paragraph: string
   request: boolean
-  type:string
+  type: string
   id: string
 }
 
-const Card = ({ title, paragraph, request , type, id}: IProps) => {
+const Card = ({ title, paragraph, request, type, id }: IProps) => {
   const [requestTicket, setRequestTicket] = useState({
     id: '',
     type: type,
     status: Status.PENDING,
     createAt: new Date(),
-    userId: id
+    userId: id,
   })
   const createTicket = useUserStore((state) => state.create.ticket)
   return (
     <div className="group py-4 px-5 grid justify-center items-center text-center gap-4 md:gap-3 rounded-md">
-      <h1 className="text-black/80 font-semibold tracking-wide font-poppins">{title}</h1>
-      <p className="text-sm leading-relaxed md:max-w-lg text-gray">{paragraph}</p>
-      
-      {request && <div>
-        <Button label="Request" color={true} handler={() => createTicket({ticket: requestTicket})}/>
-      </div>}
+      <h1 className="text-black/80 font-semibold tracking-wide font-poppins">
+        {title}
+      </h1>
+      <p className="text-sm leading-relaxed md:max-w-lg text-gray">
+        {paragraph}
+      </p>
 
-      <div className='w-0 h-1 bg-primary mt-5 rounded-full group-hover:w-full transition-all ease-in-out duration-700 cursor-pointer'></div>
+      {request && (
+        <div>
+          <Button
+            label="Request"
+            color={true}
+            handler={() => {
+              createTicket({ ticket: requestTicket })
+              console.log(requestTicket)
+            }}
+          />
+        </div>
+      )}
+
+      <div className="w-0 h-1 bg-primary mt-5 rounded-full group-hover:w-full transition-all ease-in-out duration-700 cursor-pointer"></div>
     </div>
   )
 }
