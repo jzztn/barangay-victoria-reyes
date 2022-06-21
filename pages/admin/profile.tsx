7fd418927b75044b83a4043b3c237b80c8bb43b4
jazztinecruz <jzztn.crz@gmail.com>
2022-06-21 23:39:31 +0800

    Add admin profile

--- pages/admin/profile.tsx
+++ pages/admin/profile.tsx
@@ -1,31 +1,24 @@
-import type {
-  GetServerSideProps,
-  GetStaticPaths,
-  GetStaticProps,
-  NextPage,
-} from 'next'
-import prisma from '../../adapters/prisma'
+import type { NextPage } from 'next'
 import Name from '../../components/elements/account-name'
 import Button from '../../components/elements/button'
-import Layout from '../../components/layout'
 import Header from '../../components/layout/header'
 import Main from '../../components/layout/main'
 import NavigationBar from '../../components/section/navbar'
 import NavLinks from '../../components/section/navbar/nav-links'
 import SidePanel from '../../components/section/side-panel'
 import SideBar from '../../components/styled/sidebar'
-import type { Resident, User } from '../../prisma/definition'
-import serializeData from '../../utilities/serialize-data'
 import Logo from '../../components/elements/logo'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
+import { PencilAltIcon } from '@heroicons/react/solid'
 
-interface Props {
-  users: User[]
-  residents: Resident[]
-}
-
-const Profile: NextPage<Props> = ({ users, residents }) => {
+const Profile: NextPage = () => {
+  // router
   const router = useRouter()
+  // search input field
+  const [input, setInput] = useState('')
+  const [edit, setEdit] = useState(false)
+
   return (
     <section className="h-screen grid grid-rows-[auto,1fr]">
       <Header>
@@ -73,7 +66,34 @@ const Profile: NextPage<Props> = ({ users, residents }) => {
       <Main>
         <section className="h-full grid lg:grid-cols-[auto,1fr]">
           <SidePanel image="\images\admin.png" admin={true} />
-          <section>registrations</section>
+          <section className="grid grid-rows-[auto,auto,auto,1fr] gap-8 px-10 py-7">
+            <div className='flex gap-5 items-center'>
+              <h1 className="font-semibold tracking-wide">Admin Account</h1>
+              <button onClick={() => setEdit(!edit)}>
+                <PencilAltIcon className="w-5 h-5 hover:-translate-y-1 hover:text-primary cursor-pointer transition-all duration-300" />
+              </button>
+            </div>
+
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col gap-1">
+                <h2>Username</h2>
+                <input
+                  type="text"
+                  value="admin"
+                  className="border-[1px] border-gray py-3 lg:w-96 pl-2 rounded-md"
+                />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <h2>Password</h2>
+                <input
+                  type="password"
+                  value="admin"
+                  className="border-[1px] border-gray py-3 lg:w-96 pl-2 rounded-md"
+                />
+              </div>
+            </div>
+          </section>
         </section>
       </Main>
     </section>
@@ -81,28 +101,3 @@ const Profile: NextPage<Props> = ({ users, residents }) => {
 }
 
 export default Profile
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const users = await prisma.user.findMany({
-    include: {
-      records: true,
-    },
-  })
-
-  const residents = await prisma.resident.findMany({
-    where: {
-      withId: null,
-    },
-
-    include: {
-      members: true,
-    },
-  })
-
-  return {
-    props: {
-      users: serializeData(users),
-      residents: serializeData(residents),
-    },
-  }
-}
