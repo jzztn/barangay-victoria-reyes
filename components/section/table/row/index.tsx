7869b442a48dd83c1fbf16e95b2ea77b8479d962
jazztinecruz <jzztn.crz@gmail.com>
2022-06-22 01:51:37 +0800

    Fixed table style

--- components/section/table/row/index.tsx
+++ components/section/table/row/index.tsx
@@ -1,6 +1,6 @@
 const TableRows = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-white shadow-sm shadow-gray-200 py-3 px-4 grid grid-flow-col items-center gap-5 rounded-md hover:bg-primary/30 hover:border-none hover:text-primary cursor-pointer transition-all duration-300">
+    <div className="py-3 px-4 grid grid-flow-col items-center gap-5 rounded-md hover:bg-primary/30 hover:border-none hover:text-primary cursor-pointer transition-all duration-300">
       {children}
     </div>
   )
